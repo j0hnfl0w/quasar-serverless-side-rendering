5219d7ea3bba717eafcd89fe73ae297574e4edb1
j0hnfl0w <ij69@pm.me>
2022-06-12 23:34:40 +0300

    Exports simple express app ssr

---
+++ api/ssr.js
@@ -0,0 +1,7 @@
+const serverless = require('serverless-http');
+// const app = require('./dist/index.js');
+const app = require('./src/index.js');
+// console.log('app', app);
+
+module.exports = app;
+module.exports.handler = serverless(app);
--- functions-src/render.js
+++
@@ -1,1 +0,0 @@
-module.exports.handler = require('../dist/index.js').handler;
